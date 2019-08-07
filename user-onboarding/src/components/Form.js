a5fdd12c2fcd151604191130b3eebaddebb3c4ac
Emily Lagattolla <elagattolla@gmail.com>
2019-08-07 16:22:41 -0500

    added useEffect to add users

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 
-const SignUpForm = ({ values }) => {
+const SignUpForm = ({ errors, touched, values, handleSubmit, status }) => {
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
+  useEffect(() => {
+    if (status) {
+      setUsers([...users, status]);
+    }
+  }, [status]);
 
   return (
     <div className='form'>
       <h1>Sign Up Form</h1>
       <Form>
        <Field type='text' name='name' placeholder='Name' />
+       {touched.name && errors.name && <p className="error">{errors.name}</p>}
 
        <Field type='email' name='email' placeholder='Email' />
+       {touched.email && errors.email && <p className="error">{errors.email}</p>}
 
        <Field type='password' name='password' placeholder='Password' />
+       {touched.password && errors.password && <p className="error">{errors.password}</p>}
 
        <label className='terms'>
         Accept Terms of Service
